c1366128bf31eb886b7d7aecd756127924961d85
Harini-sparkout <harinisri@sparkouttech.com>
2026-02-02 18:58:10 +0530

    Update app.ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,11 +2,14 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet, RouterLinkActive, RouterLink } from '@angular/router';
 
 import { Parent } from './parent/parent';
+import { Userform } from './userform/userform';
+import { Valid } from './valid/valid';
+
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLinkActive,RouterLink,Parent],
+  imports: [RouterOutlet, RouterLinkActive,RouterLink,Parent,Userform,Valid],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
